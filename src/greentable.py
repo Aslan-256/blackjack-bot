c86cde0f7b4b851a0efc7fab7284a98747e0eab1
ForzaElettromotrice <mingardi.federico@gmail.com>
2025-08-10 20:54:13 +0200

    Create function log() to avoid having "if verbose: print" everywhere in the code

--- src/greentable.py
+++ src/greentable.py
@@ -7,7 +7,7 @@ class GreenTable:
         self.dealer = Dealer()
         self.players = [Player()]
         self.deck = Deck()
-        self.verbose = verbose 
+        self.verbose = verbose
 
     def check_winners(self):
         results = []
@@ -21,19 +21,17 @@ class GreenTable:
             else:
                 results.append(0)
         return results
-                
+
     def dealer_turn_need(self):
         return not all(player.is_busted() for player in self.players)
 
     def start_game(self):
-        if self.verbose:
-            print("Starting a new game...")
-            print()
+        self.log("Starting a new game...\n")
 
         # Shuffle deck if needed (if the index is at or beyond the black index)
         if self.deck.need_to_shuffle():
             self.deck.shuffle_deck()
-        
+
         # Reset players and dealer
         self.players = [Player()]
         self.dealer.reset()
@@ -43,84 +41,62 @@ class GreenTable:
         self.dealer.add_card(self.deck.draw_card())
         self.players[0].add_card(self.deck.draw_card())
         self.dealer.add_card(self.deck.draw_card())
-        if self.verbose:
-            print(f"Dealer's known card: {self.dealer.get_known_card().get_value()}")
-            print(f"Player's hand: {self.players[0].get_list_of_cards()}")
-            print()
+        self.log(f"Dealer's known card: {self.dealer.get_known_card().get_value()}\nPlayer's hand: {self.players[0].get_list_of_cards()}\n")
 
         # Player's turn, possible splitted
-        if self.verbose:
-            print("Player's turn:")
-            print(f"Player's hand: {self.players[0].get_list_of_cards()}")
-        num_remaining_players = len(self.players) 
+        self.log(f"Player's turn\nPlayer's hand: {self.players[0].get_list_of_cards()}\n")
+        num_remaining_players = len(self.players)
         while num_remaining_players > 0:
             p = self.players[-1]
             num_remaining_players -= 1
             while True:
                 if p.is_busted():
-                    if self.verbose:
-                        print(f"Player busted.")
+                    self.log("Player busted.")
                     break
                 action = p.play(self.dealer.get_known_card())
                 match action:
-                    case 0: # Hit
+                    case 0:  # Hit
                         p.add_card(self.deck.draw_card())
-                        if self.verbose:
-                            print(f"Player choose to hit.")
-                            print(f"Player's hand after hit: {p.get_list_of_cards()}")
-                    case 1: # Double Down
+                        self.log(f"Player choose to hit.\nPlayer's hand after hit: {p.get_list_of_cards()}\n")
+                    case 1:  # Double Down
                         p.add_card(self.deck.draw_card())
-                        if self.verbose:
-                            print(f"Player choose to double down.")
-                            print(f"Player's hand after double down: {p.get_list_of_cards()}")
+                        self.log(f"Player choose to double down.\nPlayer's hand after double down: {p.get_list_of_cards()}\n")
                         break
-                    case 2: # Stand
-                        if self.verbose:
-                            print(f"Player choose to stand.")
+                    case 2:  # Stand
+                        self.log(f"Player choose to stand.")
                         break
-                    case 3: # Split
+                    case 3:  # Split
                         self.players.append(p.split())
                         num_remaining_players += 1
                         p.add_card(self.deck.draw_card())
                         self.players[-1].add_card(self.deck.draw_card())
-                        if self.verbose:
-                            print(f"Player choose to split.")
-                            print(f"Player's first hand: {p.get_list_of_cards()}")
-                            print(f"Player's second hand: {self.players[-1].get_list_of_cards()}")
-                
-        if self.verbose:
-            print()
+                        self.log(f"Player choose to split.\nPlayer's first hand: {p.get_list_of_cards()}\nPlayer's second hand: {self.players[-1].get_list_of_cards()}\n")
 
+        self.log("")
         # Dealer's turn
         if self.dealer_turn_need():
-            if self.verbose:
-                print("Dealer's turn:")  
-                print(f"Dealer's hand: {self.dealer.get_list_of_cards()}")  
+            self.log("Dealer's turn\nDealer's hand: " + str(self.dealer.get_list_of_cards()))
             while True:
                 if self.dealer.is_busted():
-                    if self.verbose:
-                        print(f"Dealer busted.")
+                    self.log(f"Dealer busted.")
                     break
                 action = self.dealer.play(self.dealer.get_known_card())
                 match action:
-                    case 0: # Hit
+                    case 0:  # Hit
                         self.dealer.add_card(self.deck.draw_card())
-                        if self.verbose:
-                            print(f"Dealer hits.")
-                            print(f"Dealer's hand after hit: {self.dealer.get_list_of_cards()}")
-                    case 2: # Stand
-                        if self.verbose:
-                            print(f"Dealer stands.")
-                        break            
-        if self.verbose:
-            print()
-        
-        if self.verbose:
-            print("Game ended.")
-            print(f"Dealer's hand: {self.dealer.get_list_of_cards()}")
-            for player in self.players:
-                print(f"{player._name}'s hand: {player.get_list_of_cards()}")
-            print()
-        
+                        self.log(f"Dealer hits.\nDealer's hand after hit: {self.dealer.get_list_of_cards()}\n")
+                    case 2:  # Stand
+                        self.log(f"Dealer stands.")
+                        break
+        self.log("")
+
+        self.log(f"Game ended.\nDealer's hand: {self.dealer.get_list_of_cards()}\n")
+        for i, p in enumerate(self.players):
+            self.log(f'Player {i}\'s hand: {p.get_list_of_cards()}')
+        self.log("")
+
         return self.check_winners()
-        
+
+    def log(self, message: str):
+        if self.verbose:
+            print(message)
